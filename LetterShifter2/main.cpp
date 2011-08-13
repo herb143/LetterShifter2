f4c73eda9c3a450c9df3cc0fe454dd96a5731dc6
herb143 <h.herbie@gmail.com>
2011-08-13 16:47:50 +0200

    Initial Commit

---
+++ LetterShifter2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  LetterShifter2
+//
+//  Created by Herbert Gilman on 8/13/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
