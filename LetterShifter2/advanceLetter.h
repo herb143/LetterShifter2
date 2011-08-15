2e614ff7e1482e17eacf29cc5fb16c58686b6f4a
herb143 <h.herbie@gmail.com>
2011-08-15 19:18:46 +0200

    Moved advance and retract functions to their own files.

---
+++ LetterShifter2/advanceLetter.h
@@ -0,0 +1,14 @@
+//
+//  advanceLetter.h
+//  LetterShifter2
+//
+//  Created by Herbert Gilman on 8/15/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef LetterShifter2_advanceLetter_h
+#define LetterShifter2_advanceLetter_h
+
+std::string advanceLetter(std::string sOriginal, int nLetters);
+
+#endif
---
+++ LetterShifter2/retractLetter.h
@@ -0,0 +1,14 @@
+//
+//  retractLetter.h
+//  LetterShifter2
+//
+//  Created by Herbert Gilman on 8/15/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef LetterShifter2_retractLetter_h
+#define LetterShifter2_retractLetter_h
+
+std::string retractLetter(std::string sOriginal, int nLetters);
+
+#endif
